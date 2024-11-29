c585e1e12e2ab8d68110ec0d9628cea53bbec425
Tait <165311345+mytait@users.noreply.github.com>
2024-11-29 15:05:07 +0100

    Update hf_utils.py

--- hf_utils.py
+++ hf_utils.py
@@ -3,7 +3,7 @@ import os
 from huggingface_hub import hf_hub_download
 
 
-def load_custom_model_from_hf(repo_id, model_filename="pytorch_model.bin", config_filename="config.yml", cache_dir=".checkpoints"):
+def load_custom_model_from_hf(repo_id, model_filename="pytorch_model.bin", config_filename="config.yml", cache_dir="./checkpoints"):
     os.makedirs(cache_dir, exist_ok=True)
     model_path = hf_hub_download(repo_id=repo_id, filename=model_filename, cache_dir=cache_dir)
     if config_filename is None:
